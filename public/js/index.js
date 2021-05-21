e382cd216073a689514468d44e63ef731be71b7e
salt-dev <cosmina.iosifescu@gmail.com>
2021-05-21 09:03:39 +0200

    Set-up js for keeping the buttons clicked

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,28 @@ const main = document.querySelector('#app');
 currentFunction = fizzbuzz;
 printKata(fizzbuzz);
 
-document.querySelector('#fizzbuzz').onclick = () => currentFunction = fizzbuzz;
-document.querySelector('#roman').onclick = () => currentFunction = roman;
-document.querySelector('#calculator').onclick = () => currentFunction = evaluate;
+const fizzbuzzElement = document.querySelector('#fizzbuzz');
+const romanElement = document.querySelector('#roman');
+const calculatorElement = document.querySelector('#calculator');
+const clicked = 'color: var(--lighter-grey);  text-shadow: 5px 5px var(--cool-grey)';
+const nonClicked = 'color: var(--liberty); text-shadow: none';
+
+fizzbuzzElement.onclick = () => {
+  fizzbuzzElement.style = clicked;
+  romanElement.style = nonClicked;
+  calculatorElement.style = nonClicked;
+  currentFunction = fizzbuzz;
+}
+
+romanElement.onclick = () => {
+  fizzbuzzElement.style = nonClicked;
+  romanElement.style = clicked;
+  calculatorElement.style = nonClicked;
+  currentFunction = roman;
+}
+calculatorElement.onclick = () => {
+  fizzbuzzElement.style = nonClicked;
+  romanElement.style = nonClicked;
+  calculatorElement.style = clicked;
+  currentFunction = evaluate;
+}
